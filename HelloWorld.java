841c85b5e58d32f45c687cc8a3cfd11c308bd91c
Kirsten <emiliablaise01@gmail.com>
2025-06-08 21:49:23 +0000

    This is my note to myself about these changes

---
+++ HelloWorld.java
@@ -0,0 +1,31 @@
+public class HelloWorld
+{
+    public static void main(String [] args)
+    {
+        System.out.println("Hello, World!"); // Prints Hello, World! to console
+        
+        System.out.println("Name: Kirsten Santos"); // Prints name to console
+
+        System.out.println("Favorite food: Sashimi"); // Prints favorite food to console
+
+        System.out.println("Person I'd like to meet: Amelia Earhart"); // Prints person I'd like to meet to console
+
+        System.out.println("Least favorite chore: Folding laundry"); // Prints least favorite chore to console
+
+        System.out.println("What I'd rather be doing right now: Reading my book"); // Prints what I'd rather be doing right now to console
+    }
+}
+
+/* 
+ * The main method represents the entry point for execution in a Java program. 
+ * The Java Virtual Machine loads the class containing the main method into memory and then calls it to start the program's execution.
+ */
+
+ /*
+  * System.out.println is used to display output because it is the standard way to display text on the console.
+  * "out" represents the standard output stream and "println" is a method that prints whatever text is passed to it and moves cursor to next line.
+  */
+
+  /*
+   * The purpose of comments in a program is for documentation and clarity. Knowing what each line of code does makes it easy to understand the program flow and logic.
+   */
